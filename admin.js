5d59ff28f51962211a7c1f20853371eb266b779a
89893 <161150951+89893@users.noreply.github.com>
2024-09-02 15:56:03 +0530

    Add files via upload

--- admin.js
+++ admin.js
@@ -1,143 +1,24 @@
-import TryCatch from "../middlewares/TryCatch.js";
-import { Courses } from "../models/Courses.js";
-import { Lecture } from "../models/Lecture.js";
-import { rm } from "fs";
-import { promisify } from "util";
-import fs from "fs";
-import { User } from "../models/User.js";
+import express from "express";
+import { isAdmin, isAuth } from "../middlewares/isAuth.js";
+import {
+  addLectures,
+  createCourse,
+  deleteCourse,
+  deleteLecture,
+  getAllStats,
+  getAllUser,
+  updateRole,
+} from "../controllers/admin.js";
+import { uploadFiles } from "../middlewares/multer.js";
 
-export const createCourse = TryCatch(async (req, res) => {
-  const { title, description, category, createdBy, duration, price } = req.body;
+const router = express.Router();
 
-  const image = req.file;
+router.post("/course/new", isAuth, isAdmin, uploadFiles, createCourse);
+router.post("/course/:id", isAuth, isAdmin, uploadFiles, addLectures);
+router.delete("/course/:id", isAuth, isAdmin, deleteCourse);
+router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
+router.get("/stats", isAuth, isAdmin, getAllStats);
+router.put("/user/:id", isAuth, updateRole);
+router.get("/users", isAuth, isAdmin, getAllUser);
 
-  await Courses.create({
-    title,
-    description,
-    category,
-    createdBy,
-    image: image?.path,
-    duration,
-    price,
-  });
-
-  res.status(201).json({
-    message: "Course Created Successfully",
-  });
-});
-
-export const addLectures = TryCatch(async (req, res) => {
-  const course = await Courses.findById(req.params.id);
-
-  if (!course)
-    return res.status(404).json({
-      message: "No Course with this id",
-    });
-
-  const { title, description } = req.body;
-
-  const file = req.file;
-
-  const lecture = await Lecture.create({
-    title,
-    description,
-    video: file?.path,
-    course: course._id,
-  });
-
-  res.status(201).json({
-    message: "Lecture Added",
-    lecture,
-  });
-});
-
-export const deleteLecture = TryCatch(async (req, res) => {
-  const lecture = await Lecture.findById(req.params.id);
-
-  rm(lecture.video, () => {
-    console.log("Video deleted");
-  });
-
-  await lecture.deleteOne();
-
-  res.json({ message: "Lecture Deleted" });
-});
-
-const unlinkAsync = promisify(fs.unlink);
-
-export const deleteCourse = TryCatch(async (req, res) => {
-  const course = await Courses.findById(req.params.id);
-
-  const lectures = await Lecture.find({ course: course._id });
-
-  await Promise.all(
-    lectures.map(async (lecture) => {
-      await unlinkAsync(lecture.video);
-      console.log("video deleted");
-    })
-  );
-
-  rm(course.image, () => {
-    console.log("image deleted");
-  });
-
-  await Lecture.find({ course: req.params.id }).deleteMany();
-
-  await course.deleteOne();
-
-  await User.updateMany({}, { $pull: { subscription: req.params.id } });
-
-  res.json({
-    message: "Course Deleted",
-  });
-});
-
-export const getAllStats = TryCatch(async (req, res) => {
-  const totalCoures = (await Courses.find()).length;
-  const totalLectures = (await Lecture.find()).length;
-  const totalUsers = (await User.find()).length;
-
-  const stats = {
-    totalCoures,
-    totalLectures,
-    totalUsers,
-  };
-
-  res.json({
-    stats,
-  });
-});
-
-export const getAllUser = TryCatch(async (req, res) => {
-  const users = await User.find({ _id: { $ne: req.user._id } }).select(
-    "-password"
-  );
-
-  res.json({ users });
-});
-
-export const updateRole = TryCatch(async (req, res) => {
-  if (req.user.mainrole !== "superadmin")
-    return res.status(403).json({
-      message: "This endpoint is assign to superadmin",
-    });
-  const user = await User.findById(req.params.id);
-
-  if (user.role === "user") {
-    user.role = "admin";
-    await user.save();
-
-    return res.status(200).json({
-      message: "Role updated to admin",
-    });
-  }
-
-  if (user.role === "admin") {
-    user.role = "user";
-    await user.save();
-
-    return res.status(200).json({
-      message: "Role updated",
-    });
-  }
-});
+export default router;
